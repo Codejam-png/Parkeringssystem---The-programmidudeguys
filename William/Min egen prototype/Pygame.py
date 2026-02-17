5d8ec12585b74ce87712509a3a0a5227622f4881
WilliamProgramgut <nielsenw608@gmail.com>
2026-02-17 12:49:41 +0100

    Tingting

--- William/Min egen prototype/Pygame.py
+++ William/Min egen prototype/Pygame.py
@@ -16,18 +16,25 @@ ObjectFarveListe = []
 Screenx = 600
 Screeny = 600
 ScreenSize = Screenx*Screeny
-ObjectLengthX = 10
-ObjectLengthY = 10
-ObjectSize = ObjectLengthX*ObjectLengthY
+ObjectLengthx = 60
+ObjectLengthy = 60
+ObjectAmountX = Screenx/ObjectLengthx
+ObjectAmountY = Screeny/ObjectLengthy
+ObjectSize = ObjectLengthy*ObjectLengthx
 ObjectYpos = 0
 ObjectXpos = 0
-ObjectMax = 200
 ControlPointx = 150
 ControlPointy = 150
 ControlPointWidthx = 300
 ControlPointWidthy = 300
 ControlPointSize = ControlPointWidthx*ControlPointWidthy
+PrecisionFactor = 0.15
 ScoreList = []
+NumberToLetter ={
+    "0" : "A", "1" : "B", "2" : "C", "3" : "D", "4" : "E", "5" : "F", "6" : "G", "7" : "H", "8" : "I", "9" :"J"
+}
+
+
 for i in range(int(ScreenSize/ObjectSize)): 
     object = Object(random.randrange(255),random.randrange(255),random.randrange(255))
     ObjectList.append(object) 
@@ -44,9 +51,9 @@ for i in ObjectList:
     
     ob = i
     
-    Ypos = (x*10 >= Screenx*(Ypos+10)/10 and 10) + Ypos
-    Xpos = x*10 - Screenx*Ypos/10
-    pygame.draw.rect(Window, (ob.colorR,ob.colorG,ob.colorB), [Xpos, Ypos, 10, 10], 5)
+    Ypos = (x*ObjectLengthx >= Screenx*(Ypos+ObjectLengthy)/ObjectLengthy and ObjectLengthy) + Ypos
+    Xpos = x*ObjectLengthx - Screenx*Ypos/ObjectLengthy
+    pygame.draw.rect(Window, (ob.colorR,ob.colorG,ob.colorB), [Xpos, Ypos, ObjectLengthx, ObjectLengthy], ObjectLengthx*2)
     x=x+1
 pygame.draw.rect(Window, (random.randrange(255), random.randrange(255), random.randrange(255)), [ControlPointx,ControlPointy,ControlPointWidthx,ControlPointWidthy],int(ControlPointWidthy/2))
 
@@ -57,7 +64,7 @@ pygame.image.save(Window, "William/Min egen prototype/ImageForRoconizion.png")
 with Image.open("William/Min egen prototype/ImageForRoconizion.png") as billede:
     billede = billede.convert("RGB")  # Sikrer at vi arbejder i RGB
     bredde, højde = billede.size       # Gemmer bredde og højde i pixels (size er en attribut)
-
+    rControl,gControl,bControl = billede.getpixel((ControlPointx,ControlPointy))
     PixelListe = []  # Tom liste til de nye pixels
     for y in range(højde):
         for x in range(bredde):
@@ -65,11 +72,11 @@ with Image.open("William/Min egen prototype/ImageForRoconizion.png") as billede:
             PixelFarve = [r,g,b]
             PixelListe.append(PixelFarve)
     
-    
+    ObjectPosY = 0
     for u in range(int(ScreenSize/ObjectSize)):
         ObjectPixels = []
         for z in range(ObjectSize):
-            LengthCapX = ObjectLengthX + u*ObjectLengthX
+            LengthCapX = ObjectLengthx + u*ObjectLengthx
             ObjectYpos = (z >= LengthCapX*(ObjectYpos+1)) + ObjectYpos   
             ObjectXpos = z - LengthCapX*ObjectYpos
             NumberInList = (ObjectXpos + ObjectYpos*Screenx)
@@ -80,15 +87,31 @@ with Image.open("William/Min egen prototype/ImageForRoconizion.png") as billede:
         GListe = []
         BListe = []
         FarveGennemsnit = []
+        ErrorCount = 0
         for i in range(ObjectSize):
-            RListe.append(ObjectPixels[i][0])
-            GListe.append(ObjectPixels[i][1])
-            BListe.append(ObjectPixels[i][2])
-        FarveGennemsnit.append(mean(RListe))
-        FarveGennemsnit.append(mean(GListe))
-        FarveGennemsnit.append(mean(BListe))
-        ObjectFarveListe.append(FarveGennemsnit)
-        rControl,gControl,bControl = billede.getpixel((ControlPointx,ControlPointy))
+            RPixel = ObjectPixels[i][0]
+            GPixel = ObjectPixels[i][1]
+            BPixel = ObjectPixels[i][2]
+            RListe.append(RPixel)
+            ErrorCount = ((RPixel-rControl)/rControl > 0.50 and 1 ) + ErrorCount
+            GListe.append(GPixel)
+            ErrorCount = ((GPixel-gControl)/gControl > 0.50 and 1 ) + ErrorCount
+            BListe.append(BPixel)
+            ErrorCount = ((BPixel-bControl)/bControl > 0.50 and 1 ) + ErrorCount
+        if (ErrorCount <= ObjectSize*PrecisionFactor):
+            FarveGennemsnit.append(mean(RListe))
+            FarveGennemsnit.append(mean(GListe))
+            FarveGennemsnit.append(mean(BListe))
+            ObjectPosY = (u >= ObjectAmountX*(ObjectPosY+1) and 1)+ObjectPosY
+            NumberInRow = int(u-ObjectAmountX*(ObjectPosY+1))
+            if (ObjectPosY < 10):
+                Row = NumberToLetter[f"{ObjectPosY}"] #Mangler Dict til at få det til bogstav
+            else:
+                Row = "¤"   
+            StringNumber = f"{Row}{NumberInRow}"
+            FarveGennemsnit.append(StringNumber)
+            ObjectFarveListe.append(FarveGennemsnit)
+        
     Index = 0
     for i in ObjectFarveListe:
         r = i[0]
@@ -98,10 +121,15 @@ with Image.open("William/Min egen prototype/ImageForRoconizion.png") as billede:
         gScore = abs((g-gControl)/gControl)
         bScore = abs((b-bControl)/bControl)
         FinalScore = (rScore+gScore+bScore)/3
-        if (FinalScore != 0.000):
-            ScoreList.append(FinalScore)
-        Index=Index+1
+        if (FinalScore != 0.000 and FinalScore < 0.30):
+            
+            ScoreList.append(i[3])
+            Index=Index+1
+    
+    ScoreList.sort()  
     print(ScoreList)
+    print(Index)
+    #hvis der ikke kommer mange objekter, eller slet ingen som havner på ScoreList, så fygt ej. Det er precisionFactor-variablen der afgør hvor stor precisionen skal være. Der skal være få resultater her, da farverne af kasserne er tilfældige
     
 
 
