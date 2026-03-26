97ecbd350db5a616f5075405aa451f89e547f16e
stef576a <stefan.tonnesen.bubel@gmail.com>
2026-03-26 08:59:13 +0100

    klient parkerings notifikation sat ind i Main

---
+++ Main/Databasesystem/client.py
@@ -0,0 +1,57 @@
+import socket
+import flask
+
+#DETTE ER KLIENTEN
+
+## to run in cmd write: py client.py
+
+# når klientens gps kommer tæt på parkeringspladsen skal den connecte til serveren
+#https://www.youtube.com/watch?v=ZVfeIWk6974&t=578s
+
+#klienten connecter til serveren, sender preference og modtager den bedste plads.
+
+app = flask.Flask(__name__)
+
+
+@app.route('/', methods=['POST','GET'])
+def askforlocation():
+    global pladspreference
+    pladspreference = str(flask.request.form.get('pladspref'))
+    return flask.render_template('askforlocation.html', pladsprefHTML = pladspreference)
+
+
+@app.route('/locationdata', methods=['POST'])
+def locationdata():
+    #pladspreference = flask.request.form['pladspreference']
+    latitude = flask.request.form.get('latitude')
+    longitude = flask.request.form.get('longitude')
+    print(f"location er latitude: {latitude} og longitude: {longitude}")
+    if vedParkeringPladsen(latitude, longitude):
+        message = connect_and_receive()
+        return flask.render_template('notification.html', message=message)
+    else:
+        return flask.render_template('askforlocation.html', LOCmessage = "Du er udenfor rækkevidde")
+
+
+def vedParkeringPladsen(latitude, longitude):#sammenlign latitude og longitude med parkeringspladsens koordinater
+    if float(latitude) <= 56.147890 and float(latitude) >= 56.146808 and float(longitude) >= 8.990583 and float(longitude) <= 8.995449: #er kommet hertil
+        return True
+    else:
+        return False
+
+def connect_and_receive():
+    s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+    s.connect((socket.gethostname(), 8080))
+    global pladspreference #referer til den preference som er sat i app route /.
+    s.send(bytes(pladspreference, "utf-8"))
+    full_msg = ''
+    while True:
+        msg = s.recv(8)
+        if not msg:
+            break
+        full_msg += msg.decode("utf-8")
+    print(full_msg)
+    s.close()
+    return full_msg
+
+app.run(debug=True)
---
+++ Main/Databasesystem/parkeringsSystem.py
@@ -0,0 +1,95 @@
+import socket
+import sqlite3
+
+
+#DETTE ER SERVEREREN
+# skab et end point som klienten kan connecte til, og sende data frem og tilbage
+s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 8080))
+s.listen(5)
+# programmet afventer en forbindelse fra klienten.
+
+    #funktion til at oprette forbindelse til databasen og returnere en connection object som kan bruges i andre funktioner
+def get_db_connection():
+    conn = sqlite3.connect("ParkeringsDatabase.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def send_notification(message, client_socket):    
+     client_socket.send(bytes(message, "utf-8"))
+
+    #funktion til at finde den bedste plads baseret på brugerpræferencen og returnere en besked som kan sendes til klienten.
+def bedstePladsOgBesked(brugerpreference, conn):    
+
+    # hvis præferencen på en måde skulle være None gives der stadig svar
+    if brugerpreference == "None":
+        if conn.execute("SELECT BLokationKode FROM ParkeringsBås WHERE Type = 'almindelig' AND Ledig = 1 ORDER BY AfstandFI LIMIT 1;").fetchone() != None: #Henter data fra databasen og ser om der findes noget på den prefererede plads som sql-sætningen definerer. Hvis der gør det så fortsæt, hvis ikke så sig "ingen plads" (i return sætningen)
+            plads = conn.execute("SELECT BLokationKode FROM ParkeringsBås WHERE Type = 'almindelig' AND Ledig = 1 ORDER BY AfstandFI LIMIT 1;").fetchone()[0]
+            message = f"Du har ikke indtastet en preference, du faar derfor givet den tilgaengelige almindelige plads der er taettest paa skolens indgang:{plads}"
+            return message
+        elif conn.execute("SELECT BLokationKode FROM ParkeringsBås WHERE Type = 'almindelig' AND Ledig = 1 ORDER BY AfstandFI LIMIT 1;").fetchone() == None:
+            message = "Der er desvaerre ingen ledige pladser af nogen type."
+            return message
+
+    #almindelig
+    if brugerpreference == "Almindelig":
+        if conn.execute("SELECT BLokationKode FROM ParkeringsBås WHERE Type = 'almindelig' AND Ledig = 1 ORDER BY AfstandFI LIMIT 1;").fetchone() != None: #Henter data fra databasen og ser om der findes noget på den prefererede plads som sql-sætningen definerer. Hvis der gør det så fortsæt, hvis ikke så sig "ingen plads" (i return sætningen)
+            plads = conn.execute("SELECT BLokationKode FROM ParkeringsBås WHERE Type = 'almindelig' AND Ledig = 1 ORDER BY AfstandFI LIMIT 1;").fetchone()[0]
+            message = f"Der er en ledig plads paa {plads}"
+            return message
+        elif conn.execute("SELECT BLokationKode FROM ParkeringsBås WHERE Type = 'almindelig' AND Ledig = 1 ORDER BY AfstandFI LIMIT 1;").fetchone() == None:
+            message = "Der er desvaerre ingen ledige pladser af nogen type."
+            return message
+
+    #elbil
+    elif brugerpreference == "Elbil":
+        if conn.execute("SELECT BLokationKode FROM ParkeringsBås WHERE Type = 'elbil' AND Ledig = 1 ORDER BY AfstandFI LIMIT 1;").fetchone() != None:
+            plads = conn.execute("SELECT BLokationKode FROM ParkeringsBås WHERE Type = 'elbil' AND Ledig = 1 ORDER BY AfstandFI LIMIT 1;").fetchone()[0] #Vi henter dataen ud fra select sætningen. Grunden til [0] er for at vi kun vælger "sqlite3.Row object"(tror vi) og dataen vises korrekt, syntaksen er fundet her: https://stackoverflow.com/questions/76354183/database-returns-sqlite3-row-object-at-0x0000019494a725f0-instead-of-data
+            
+            message = f"Der er en ledig elbilplads paa {plads}"
+            return message
+        
+        if conn.execute("SELECT BLokationKode FROM ParkeringsBås WHERE Type = 'elbil' AND Ledig = 1 ORDER BY AfstandFI LIMIT 1;").fetchone() == None: #Henter data fra databasen og ser om der findes noget på den prefererede plads som sql-sætningen definerer. Hvis der ikke gør det (==None) så søger den istedet i almindelig og giver den tilgængelige almindelige plads
+            plads = conn.execute("SELECT BLokationKode FROM ParkeringsBås WHERE Type = 'almindelig' AND Ledig = 1 ORDER BY AfstandFI LIMIT 1;").fetchone()[0]
+            message = f"Der er desværre ingen ledige elbilpladser pt. Den bedste alternative plads er {plads}"
+            return message
+        elif conn.execute("SELECT BLokationKode FROM ParkeringsBås WHERE Type = 'almindelig' AND Ledig = 1 ORDER BY AfstandFI LIMIT 1;").fetchone() == None:
+            message = "Der er desvaerre ingen ledige pladser af nogen type."
+            return message
+    #handicap
+    elif brugerpreference == "Handicap":
+        if conn.execute("SELECT BLokationKode FROM ParkeringsBås WHERE Type = 'handicap' AND Ledig = 1 ORDER BY AfstandFI LIMIT 1;").fetchone() != None: #Henter data fra databasen og tjekker om der findes noget på den prefererede plads som sql-sætningen definerer. Hvis der gør det så fortsæt, hvis ikke så gå ned i elif som søger almindelige.
+            plads = conn.execute("SELECT BLokationKode FROM ParkeringsBås WHERE Type = 'handicap' AND Ledig = 1 ORDER BY AfstandFI LIMIT 1;").fetchone()[0] #Vi henter dataen ud fra select sætningen. Grunden til [0] er for at vi kun vælger "sqlite3.Row object"(tror vi) og dataen vises korrekt, syntaksen er fundet her: https://stackoverflow.com/questions/76354183/database-returns-sqlite3-row-object-at-0x0000019494a725f0-instead-of-data
+            message = f"Der er en ledig handicapplads paa {plads}"
+            return message
+        
+        elif conn.execute("SELECT BLokationKode FROM ParkeringsBås WHERE Type = 'handicap' AND Ledig = 1 ORDER BY AfstandFI LIMIT 1;").fetchone() == None: #Henter data fra databasen og ser om der findes noget på den prefererede plads som sql-sætningen definerer. Hvis der ikke gør det (==None) så søger den istedet i almindelig og giver den tilgængelige almindelige plads
+            plads = conn.execute("SELECT BLokationKode FROM ParkeringsBås WHERE Type = 'almindelig' AND Ledig = 1 ORDER BY AfstandFI LIMIT 1;").fetchone()[0]
+            message = f"Der er desvaerre ingen ledige handicappladser pt. Den bedste alternative plads er {plads}"
+            return message
+        elif conn.execute("SELECT BLokationKode FROM ParkeringsBås WHERE Type = 'almindelig' AND Ledig = 1 ORDER BY AfstandFI LIMIT 1;").fetchone() == None:
+            message = "Der er desvaerre ingen ledige pladser af nogen type."
+            return message
+    
+
+
+# programmet skal afvente input fra brugeren på internettet
+def main():
+    while True:
+            # når en klient connecter til serveren, så accepters forbindelsen og der modtages data om clienten (client_socket, addresse og brugerpræference)
+        client_socket, addr = s.accept()
+        print(f"Forbundet til {addr}")
+        conn = get_db_connection()
+        brugerpreference = client_socket.recv(1024).decode("utf-8")
+        print(f"Brugerpræference: {brugerpreference}")
+
+            #severen gennemsøger databasen for at finde den bedste plads baseret på brugerpræferencen, sender en notifikation tilbage til klienten og lukker forbindelsen.
+        message = bedstePladsOgBesked(brugerpreference,conn)
+        send_notification(message, client_socket)
+        print(f"Sendt besked til {addr}: {message}")
+        conn.close()
+        client_socket.close()
+        print(f"Afsluttet forbindelse til {addr}")
+if __name__ == "__main__": 
+    main()        
+            
